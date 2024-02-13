2dded5fd0efad3368f59bb8702be4e9d80a74dd7
sayadennis <sayarenedennis@gmail.com>
2024-02-12 18:02:18 -0600

    start to refactor to use for nmf

--- ClassicalML/nested.py
+++ ClassicalML/nested.py
@@ -72,62 +72,89 @@ def run(
             X_train, X_test = X.iloc[train_index, :], X.iloc[test_index, :]
             y_train, y_test = y.iloc[train_index], y.iloc[test_index]
             # Run grid search on layer 1 training set (second layer of split)
-            scaler = StandardScaler()
-            # clf = RandomForestClassifier(
-            #    n_estimators=300,
-            #    criterion="gini",
-            #    class_weight="balanced",
-            #    random_state=seed,
-            # )
-            clf = xgb.XGBClassifier(
-                objective="reg:logistic",
-                subsample=1,
-                reg_alpha=0,
-                reg_lambda=1,
-                n_estimators=300,
-                seed=seed,
+            gsCV = gs_hparam(X_train, y_train, seed=seed)
+            performance = record_performance(
+                gsCV, X_test, y_test, performance, fold=i, split=split
             )
-            pipe = Pipeline([("scaler", scaler), ("classifier", clf)])
-            gsCV = GridSearchCV(
-                pipe,
-                param_grid=PARAM_DIST,
-                n_jobs=N_CPU,
-                scoring=SCORING_METRIC,
-                refit=True,
-                cv=StratifiedKFold(n_splits=5, random_state=seed + 1, shuffle=True),
-            )
-            gsCV.fit(X_train, y_train)
-            opt_params = gsCV.best_params_
-            opt_mean_score = np.mean(
-                gsCV.cv_results_["mean_test_score"][
-                    np.all(
-                        [
-                            (gsCV.cv_results_[f"param_{param_name}"] == param_vals)
-                            for param_name, param_vals in opt_params.items()
-                        ],
-                        axis=0,
-                    )
-                ]
-            )
-            performance.loc[f"Fold {i}", f"CV ROC {split}"] = opt_mean_score
-            y_pred = gsCV.predict(X_test)
-            y_prob = gsCV.predict_proba(X_test)
-            if y_prob.shape[1] == 2:  # if binary class
-                y_prob = y_prob[:, 1]
-            test_roc = metrics.roc_auc_score(y_test, y_prob, multi_class="ovr")
-            test_prec = metrics.precision_score(y_test, y_pred)
-            test_recall = metrics.recall_score(y_test, y_pred)
-            test_f1 = metrics.f1_score(y_test, y_pred)
-            performance.loc[f"Fold {i}", f"Test ROC {split}"] = test_roc
-            performance.loc[f"Fold {i}", f"Test Precision {split}"] = test_prec
-            performance.loc[f"Fold {i}", f"Test Recall {split}"] = test_recall
-            performance.loc[f"Fold {i}", f"Test F1 {split}"] = test_f1
+    # Calculate average across folds
     performance.loc["Average", :] = (
         performance.loc[[f"Fold {l}" for l in np.arange(folds)], :]
         .mean(axis=0)
         .values.ravel()
     )
     performance.to_csv(outfn)
+
+
+def gs_hparam(X: pd.DataFrame, y: pd.DataFrame, seed: int) -> GridSearchCV:
+    """
+    Run GridSearch with default model (XGB) and defined hyperparameters.
+
+    Parameters:
+        X: input.
+        y: target.
+
+    Returns:
+        The post-gridsearch GridSearchCV object.
+    """
+    scaler = StandardScaler()
+    clf = xgb.XGBClassifier(
+        objective="reg:logistic",
+        subsample=1,
+        reg_alpha=0,
+        reg_lambda=1,
+        n_estimators=300,
+        seed=seed,
+    )
+    pipe = Pipeline([("scaler", scaler), ("classifier", clf)])
+    gsCV = GridSearchCV(
+        pipe,
+        param_grid=PARAM_DIST,
+        n_jobs=N_CPU,
+        scoring=SCORING_METRIC,
+        refit=True,
+        cv=StratifiedKFold(n_splits=5, random_state=seed + 1, shuffle=True),
+    )
+    gsCV.fit(X, y)
+    return gsCV
+
+
+def record_performance(
+    gsCV: GridSearchCV,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,
+    performance: pd.DataFrame,
+    fold: int,
+    split: int,
+) -> pd.DataFrame:
+    """
+    Record performance of the grid search process.
+    """
+    opt_params = gsCV.best_params_
+    opt_mean_score = np.mean(
+        gsCV.cv_results_["mean_test_score"][
+            np.all(
+                [
+                    (gsCV.cv_results_[f"param_{param_name}"] == param_vals)
+                    for param_name, param_vals in opt_params.items()
+                ],
+                axis=0,
+            )
+        ]
+    )
+    performance.loc[f"Fold {fold}", f"CV ROC {split}"] = opt_mean_score
+    y_pred = gsCV.predict(X_test)
+    y_prob = gsCV.predict_proba(X_test)
+    if y_prob.shape[1] == 2:  # if binary class
+        y_prob = y_prob[:, 1]
+    test_roc = metrics.roc_auc_score(y_test, y_prob, multi_class="ovr")
+    test_prec = metrics.precision_score(y_test, y_pred)
+    test_recall = metrics.recall_score(y_test, y_pred)
+    test_f1 = metrics.f1_score(y_test, y_pred)
+    performance.loc[f"Fold {fold}", f"Test ROC {split}"] = test_roc
+    performance.loc[f"Fold {fold}", f"Test Precision {split}"] = test_prec
+    performance.loc[f"Fold {fold}", f"Test Recall {split}"] = test_recall
+    performance.loc[f"Fold {fold}", f"Test F1 {split}"] = test_f1
+    return performance
 
 
 if __name__ == "__main__":
